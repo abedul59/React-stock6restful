8b07db0d48c07d52eac0394685bce56c4aff7cc6
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-05-15 12:44:14 +0800

    20c

--- src/GetStock6Sign.js
+++ src/GetStock6Sign.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 
 
 const GetStock6Sign = () => {
+
     const [stock_id, setStock_id] = useState("");
     const [stock_name, setStock_name] = useState("");
     const [url, setUrl] = useState('');
@@ -13,7 +14,33 @@ const GetStock6Sign = () => {
         setStock_id(stock_id);
         
         setUrl(`https://stock6-restfulex.onrender.com/api/Stock6Sign202304/getstockinfo/${stock_id}`);
-        console.log(`最新得到網址為${url}`);
+        console.log(`最新得到網址a為${url}`);
+        /*
+        fetch(url) 
+        .then(data => data.json())
+        .then(response => {
+          const {cStockName, cNewestSeason} = response;
+          console.log(cStockName);
+          console.log(cNewestSeason);
+          */
+
+        setStock_name(cStockName);
+        //console.log(`1. 最新得到股票名稱為${event.target.value}`);
+        console.log(`2. 最新得到股票名稱為${stock_id}`);
+          
+          
+        };
+          
+      
+
+      
+
+const handleClick = () => {
+        //event.preventDefault();
+        //setStock_id(stock_id);
+        
+        //setUrl(`https://stock6-restfulex.onrender.com/api/Stock6Sign202304/getstockinfo/${stock_id}`);
+        console.log(`最新得到網址b為${url}`);
         
         fetch(url) 
         .then(data => data.json())
@@ -31,8 +58,6 @@ const GetStock6Sign = () => {
       
 
       }
-
-
 
 
 
@@ -59,6 +84,8 @@ return (
 
 
     </form>
+
+    <button type="buton" onClick = {handleClick}>Renew</button>
 
     <div>
       <h1>{url}</h1>
