6345d5cf73143dd9d03a8173e34cab97bb667941
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-05-15 13:45:35 +0800

    25c

--- src/GetStock6Sign.js
+++ src/GetStock6Sign.js
@@ -7,6 +7,8 @@ const GetStock6Sign = () => {
 
     const [stock_id, setStock_id] = useState("");
     const [stock_name, setStock_name] = useState("");
+    const [newest_season, setNewest_season] = useState("");
+
     const [url, setUrl] = useState('');
 
     const handleSubmit = (event) => {
@@ -14,20 +16,7 @@ const GetStock6Sign = () => {
         setStock_id(stock_id);
         
         setUrl(`https://stock6-restfulex.onrender.com/api/Stock6Sign202304/getstockinfo/${stock_id}`);
-        console.log(`最新得到網址a為${url}`);
-        /*
-        fetch(url) 
-        .then(data => data.json())
-        .then(response => {
-          const {cStockName, cNewestSeason} = response;
-          console.log(cStockName);
-          console.log(cNewestSeason);
-          */
 
-        //setStock_name(cStockName);
-        //console.log(`1. 最新得到股票名稱為${event.target.value}`);
-        //console.log(`2. 最新得到股票名稱為${stock_id}`);
-          
           
         };
           
@@ -40,7 +29,7 @@ const handleClick = () => {
         //setStock_id(stock_id);
         
         //setUrl(`https://stock6-restfulex.onrender.com/api/Stock6Sign202304/getstockinfo/${stock_id}`);
-        console.log(`最新得到網址b為${url}`);
+        console.log(`最新得到網址為${url}`);
         
         fetch(url) 
         .then(data => data.json())
@@ -50,8 +39,9 @@ const handleClick = () => {
           console.log(cNewestSeason);
 
         setStock_name(cStockName);
+        setNewest_season(cNewestSeason);
         //console.log(`1. 最新得到股票名稱為${event.target.value}`);
-        console.log(`2. 最新得到股票名稱為${stock_id}`);
+        //console.log(`2. 最新得到股票名稱為${stock_id}`);
           
           
         });  
@@ -81,7 +71,7 @@ return (
         onChange={(event) => {
           setStock_id(event.target.value);
           console.log(`1. 最新股票代號為${event.target.value}`);
-          console.log(`2. 最新股票代號為${stock_id}`);
+          //console.log(`2. 最新股票代號為${stock_id}`);
         }
         }
       />
@@ -97,6 +87,7 @@ return (
       <h1>{url}</h1>
         <h1>{stock_id}</h1>
         <h1>{stock_name}</h1>
+        <h1>{newest_season}</h1>
     </div>
     </div>
   );
