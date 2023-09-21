be4931529a0e570be4ca736894794cb5ba1caa32
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-09-21 12:46:00 +0800

    Sign 3

---
+++ src/Sign3Detail.js
@@ -0,0 +1,56 @@
+import React from "react";
+//import './App.css'
+
+const Sign3Detail = (props) => {
+
+      
+return (
+    <div>
+ 
+
+    <div>
+        <table>
+        <tr><td>稅後淨利指標</td></tr>
+        <tr>
+        <td><h1>{props.stock6data.cb1N}</h1></td>
+        <td><h1>{props.stock6data.cb1}</h1></td>
+        </tr>
+
+        <tr>
+        <td><h1>{props.stock6data.cb2N}</h1></td>
+        <td><h1>{props.stock6data.cb2}</h1></td>
+        </tr>
+
+        <tr>
+        <td><h1>{props.stock6data.cb3N}</h1></td>
+        <td><h1>{props.stock6data.cb3}</h1></td>
+        </tr>
+
+        <tr>
+        <td><h1>{props.stock6data.cb4N}</h1></td>
+        <td><h1>{props.stock6data.cb4}</h1></td>
+        </tr>
+
+        <tr>
+        <td><h1>{props.stock6data.cb5N}</h1></td>
+        <td><h1>{props.stock6data.cb5}</h1></td>
+        </tr>
+
+        <tr>
+        <td><h1>{props.stock6data.cb6N}</h1></td>
+        <td><h1>{props.stock6data.cb6}</h1></td>
+        </tr>
+        
+
+        <tr>
+        <td><h1>y最新一季YoY</h1></td>
+        <td><h1></h1></td>
+        </tr>
+        </table>
+
+    </div>
+
+    </div>
+  );
+};
+export default Sign3Detail;
